91bd018de297912fc9d1dc5771300e959f106eac
Mbalenhle <mbalyngema@gmail.com>
2023-07-17 13:38:17 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <studio>
+/**
+ *main - A program that print the size of various of compter types
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\",(unsighed long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n",(unsighed long)sizof(b));
+printf("Size of a long int: %lu byte(s)\n",(unsighed long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n",(unsighedlong)sizeof(d));
+printf("Size of a float; %lu byt(s)\n",(unsighed long)sizeof(f));
+return (0);
+}
